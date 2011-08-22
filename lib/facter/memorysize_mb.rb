c65f2eec520126d8aedf9b1854dc37b46fe58fac
Brian L. Troutwine <brian@troutwine.us>
2011-08-22 02:33:42 -0400

    For reasons I do not understand, my facts are not available in my templates. Gone simple, hope to have corrected issue.
    Signed-off-by: Brian L. Troutwine <brian@troutwine.us>

--- lib/facter/memorysize_mb.rb
+++ lib/facter/memorysize_mb.rb
@@ -1,30 +1,16 @@
 # As seen in: http://groups.google.com/group/puppet-users/browse_thread/thread/f1c0689dad937853/095d3109f1a8b284
 
 require 'facter'
-
-{
-  :MemorySizeRaw => "MemTotal",
-  :MemoryFreeRaw => "MemFree",
-  :SwapSizeRaw   => "SwapTotal",
-  :SwapFreeRaw   => "SwapFree"
-}.each do |fact, name|
-
-  Facter.add(fact) do
-    confine :kernel => :linux
-    setcode do
-      memsize_raw = ""
-      Thread::exclusive do
-        File.readlines("/proc/meminfo").each do |l|
-          memsize_raw = $1.to_i if l =~ /^#{name}:\s+(\d+)\s+\S+/
-          # MemoryFree == memfree + cached + buffers
-          #  (assume scales are all the same as memfree)
-          if name == "MemFree" &&
-              l =~ /^(?:Buffers|Cached):\s+(\d+)\s+\S+/
-            memsize_raw += $1.to_i
-          end
-        end
-      end
-      memsize_raw
-    end
+Facter.add("memorysize_mb") do
+  confine :kernel => :Linux
+  ram = 0
+  # Steal linux's meminfo
+  File.open( "/proc/meminfo" , 'r' ) do |f|
+    f.grep( /^MemTotal:/ ) { |mem|
+      ram = mem.split( / +/ )[1].to_i / 1024
+    }
+  end
+  setcode do
+    ram
   end
 end
